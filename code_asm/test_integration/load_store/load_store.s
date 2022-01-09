8232a2f1f94400da903e5e0ae6619c07e4c1ed9d
Tom Niget <tom.niget@etu.unice.fr>
2022-01-09 13:41:29 +0000

    load_store.s edited online with Bitbucket

--- code_asm/test_integration/load_store/load_store.s
+++ code_asm/test_integration/load_store/load_store.s
@@ -7,6 +7,6 @@ sub sp, #4
 ldr r2,[sp, #4]
 @r0 value should be 170, AA, 
 @r1 and r2 values should be 255, FF,
-@In ROM, 04 value should be 255, FF, 
-@In ROM, 05 value should be 170, AA, 
+@In RAM, 04 value should be 255, FF, 
+@In RAM, 05 value should be 170, AA, 
 @Needs shift_add_sub_move and sp_address to be implemented
